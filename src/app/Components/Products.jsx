bd480a933acbdb7c2731c9348b452c59ce8c135f
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-08-06 22:54:08 +0000

    created file

---
+++ src/app/Components/Products.jsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+export default function Products() {
+    const [products, setProducts] = useState([]);
+    
+    return (
+      <>
+      </>
+    )
+  }
+  
