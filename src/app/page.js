7dfda5e94848652f97e18272c0e75179718b64c1
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-08-14 01:05:02 +0000

    conncected to backend

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,17 @@
+'use client'
+
+import { useEffect } from "react";
 import CarouselParent from "./Components/CarouselParent";
 import Header from "./Components/Header";
+import axios from "axios";
 
 
 export default function Home() {
+  useEffect(() => {
+    async function wakeupserver() {
+      const response = await axios.get('https://kick-cartel-backend.onrender.com');
+    }
+  })
   return (
     <main>
     <Header/> 
