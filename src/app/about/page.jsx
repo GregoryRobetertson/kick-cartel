ce4019fd211b94aab0f08c01e160c286184f66cd
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-10-02 18:52:27 +0000

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Components/Header';
+import About from './About';
+
+
+
+export default function page() {
+    return ( 
+        <>
+         <Header/>
+        <main >
+         <About/>
+        </main>
+        </>
+   
+    )
+}
